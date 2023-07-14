ed6d84b11f674e2bc0cd9b8ec8bc0cd905a4d21b
Amirbekov Anis Pulodovich <anisamirbekov3@gmail.com>
2023-07-14 10:41:33 +0500

    first commit

---
+++ aiogramBot.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+
+API_TOKEN = "5870105739:AAEkQVYMG6AINTxmagdROkvfjsnaWT2J56U"
+bot = Bot(API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def echo_bot(message: types.Message):
+    await message.answer(message.text)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
